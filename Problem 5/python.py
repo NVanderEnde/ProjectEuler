c4907bfbdad53f55efb8015f1ef47c3f3313ec14
Nicholas Vander Ende <nvanderende@gmail.com>
2012-11-10 16:01:16 -0500

    Solution to Problem 5
    11/10/12

---
+++ Problem 5/python.py
@@ -0,0 +1,52 @@
+#!C:/Python27
+#Contact: nvanderende@gmail.com
+
+"""2520 is the smallest number that can be divided by each of the numbers from 
+1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the 
+numbers from 1 to 20?"""
+
+#Brute force method:
+#Start the count at 20 and increment in steps of 20 since 20 is the largest
+#denominator, it must be the largest factor
+
+count = 20
+list = [ ]
+for x in xrange(1, 21):
+	list.append(x)
+"""
+while 1:
+	win = 0
+	for x in list:
+		if count % x == 0:
+			if x == 20:
+				print count, "succeeded"
+				win = 1
+			else:
+				continue
+		else:
+			#print count, "failed."
+			break
+	if win == 1:
+		break
+	count += 20
+	
+print count
+"""
+
+#The more efficient method that makes use of factorial properties
+#Creates the number, rather than finding it, by iteratively generating the 
+#lowest common multiples for each successive number
+
+def greatest_common_denominator(x,y): 
+	return b and greatest_common_denominator(y, x % y) or x
+def lowest_common_multiple(x,y): 
+	return x * y / greatest_common_denominator(x, y)
+
+
+value=1
+for x in xrange(1, 21):
+	value = lowest_common_multiple(value, x)
+
+print value
