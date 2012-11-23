e4b53987b22e73d28aa32f273c912a9e16e62159
Nicholas Vander Ende <nvanderende@gmail.com>
2012-11-22 21:18:12 -0500

    Solution to Problem 3
    Improved documentation

--- Problem 3/python.py
+++ Problem 3/python.py
@@ -10,13 +10,8 @@ What is the largest prime factor of the number 600851475143 ?
 Taking the following approach: Starting with the first non-1 prime, check to
 see if it's a factor. If it is, reassign to that value and check the new value
 against the next integer value for factorization (in this case, 600851475143).
-This works because eventually you work down to a factor of n which cannot be 
-divided (i keeps increasing) and then you hit the sqrt(n) upper bound, and 
-you're done.
-
-Iterating through 600 billion values would take, ehh, a long time, as would
-checking each iteration for factorization and prime-ness. This is considerably
-more efficient."""
+Once a factor is found, we continually factor it into our value until we reach 
+the point where it can be factored no more - thus, the greatest prime factor."""
 
 number = 600851475143
 def prime_factor(value):
